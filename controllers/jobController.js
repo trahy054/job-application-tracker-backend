3bae9d4cfeeb1e306212abfd0873b36dd97436f7
Son Hoang Tran <tranhoangson0601@gmail.com>
2026-02-23 14:17:40 +1030

    Add update job functionality

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,9 +1,11 @@
 let jobs = [];
 
+// Get all jobs
 const getAllJobs = (req, res) => {
   res.json(jobs);
 };
 
+// Add a new job
 const addJob = (req, res) => {
   const newJob = req.body;
   newJob.id = jobs.length + 1;
@@ -11,6 +13,7 @@ const addJob = (req, res) => {
   res.status(201).json(newJob);
 };
 
+// Delete a job by ID
 const deleteJob = (req, res) => {
   const id = parseInt(req.params.id);
 
@@ -19,8 +22,25 @@ const deleteJob = (req, res) => {
   res.json({ message: "Job deleted successfully" });
 };
 
+// Update a job by ID
+const updateJob = (req, res) => {
+  const id = parseInt(req.params.id);
+  const {company , position, status} = req.body;
+
+  const job = jobs.find(j => j.id === parseInt(id));
+  if (!job) return res.status(404).json({ message: "Job not found" });
+
+  if (company) job.company = company;
+  if (position) job.position = position;
+  if (status) job.status = status;
+
+  res.json(job);
+}
+
+// Export the controller functions
 module.exports = {
     getAllJobs,
     addJob,
-    deleteJob
+    deleteJob,
+    updateJob
 };
--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getAllJobs, addJob, deleteJob } = require("../controllers/jobController");
+const { getAllJobs, addJob, deleteJob, updateJob } = require("../controllers/jobController");
 
 router.get("/jobs", getAllJobs);
 router.post("/jobs", addJob);
 router.delete("/jobs/:id", deleteJob);
-
+router.put("/jobs/:id", updateJob);
 
 
 module.exports = router;
